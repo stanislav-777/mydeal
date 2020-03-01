0ff7976d2f0595c06b7348a5d569dc87a03d2f77
stanislav-777 <57234838+stanislav-777@users.noreply.github.com>
2020-03-01 20:07:57 +0300

    сделано 2 задание

--- index.php
+++ index.php
@@ -71,8 +71,12 @@ $show_complete_tasks = rand(0, 1);
                     </nav>
 
                     <label class="checkbox">
-                        <!--добавить сюда атрибут "checked", если переменная $show_complete_tasks равна единице-->
-                        <input class="checkbox__input visually-hidden show_completed" type="checkbox">
+                        <!--добавить сюда атрибут "checked", если переменная $show_complete_tasks равна единице -->
+                        <?php if($show_complete_tasks == 1)
+                        {echo '<input class="checkbox__input visually-hidden" type="checkbox" checked>';}
+                            else {'<input class="checkbox__input visually-hidden" type="checkbox">';}
+                        
+                        ?>
                         <span class="checkbox__text">Показывать выполненные</span>
                     </label>
                 </div>
@@ -92,7 +96,25 @@ $show_complete_tasks = rand(0, 1);
 
                         <td class="task__date"></td>
                     </tr>
-                    <!--показывать следующий тег <tr/>, если переменная $show_complete_tasks равна единице-->
+                    <?php 
+                    if($show_complete_tasks == 1){
+                        echo '
+                        <tr class="tasks__item task task--completed">
+                        <td class="task__select">
+                            <label class="checkbox task__checkbox">
+                                <input class="checkbox__input visually-hidden" type="checkbox" checked>
+                                <span class="checkbox__text">Записаться на интенсив "Базовый PHP"</span>
+                            </label>
+                        </td>
+                        <td class="task__date">10.10.2019</td>
+                        <td class="task__controls"></td>
+                    </tr>
+                        ';
+                    }
+                    else {
+                        echo '';
+                    }
+                    ?>
                 </table>
             </main>
         </div>
