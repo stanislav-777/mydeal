c4ad8510d336915646a67311cd7015f1dbba436d
stanislav-777 <57234838+stanislav-777@users.noreply.github.com>
2020-03-03 22:43:49 +0300

    4.5 шаблонизация и работа с датой

---
+++ templates/main.php
@@ -0,0 +1,100 @@
+<section class="content__side">
+                <h2 class="content__side-heading">Проекты</h2>
+
+                <nav class="main-navigation">
+                    <ul class="main-navigation__list">
+                        <?php
+                        foreach ($masProject as $value){
+                            echo '<li class="main-navigation__list-item">
+                            <a class="main-navigation__list-item-link" href="#">'.$value.'</a>
+                            <span class="main-navigation__list-item-count">'.numberOfTasks($masTask,$value).'</span>
+                            </li>';
+                        }
+                        
+                        ?>
+                    </ul>
+                </nav>
+
+                <a class="button button--transparent button--plus content__side-button"
+                   href="pages/form-project.html" target="project_add">Добавить проект</a>
+            </section>
+
+            <main class="content__main">
+                <h2 class="content__main-heading">Список задач</h2>
+
+                <form class="search-form" action="index.php" method="post" autocomplete="off">
+                    <input class="search-form__input" type="text" name="" value="" placeholder="Поиск по задачам">
+
+                    <input class="search-form__submit" type="submit" name="" value="Искать">
+                </form>
+
+                <div class="tasks-controls">
+                    <nav class="tasks-switch">
+                        <a href="/" class="tasks-switch__item tasks-switch__item--active">Все задачи</a>
+                        <a href="/" class="tasks-switch__item">Повестка дня</a>
+                        <a href="/" class="tasks-switch__item">Завтра</a>
+                        <a href="/" class="tasks-switch__item">Просроченные</a>
+                    </nav>
+
+                    <label class="checkbox">
+                        <?php if($show_complete_tasks == 1)
+                        {echo '<input class="checkbox__input visually-hidden" type="checkbox" checked>';}
+                            else {'<input class="checkbox__input visually-hidden" type="checkbox">';}
+                        
+                        ?>
+                        <span class="checkbox__text">Показывать выполненные</span>
+                    </label>
+                </div>
+
+                <table class="tasks">
+                        <?php
+                        $flagTaskImportant = '';
+                        for($i=0;$i<6;$i++){
+                                if ($masTask[$i]['done']){
+                                    if ($show_complete_tasks){
+                                        if ((strtotime($masTask[$i]['date']) - time()) <=24*3600) {
+                                            $flagTaskImportant = 'task--important';
+                                        } else {
+                                            $flagTaskImportant = '';
+                                        }
+                                echo '<tr class="tasks__item task task--completed '.$flagTaskImportant.'">
+                                <td class="task__select">
+                                <label class="checkbox task__checkbox"><input class="checkbox__input visually-hidden task__checkbox" type="checkbox" value="1">
+                                <span class="checkbox__text">'.$masTask[$i]['task'].'</span>
+                                </label>
+                                </td>
+                                <td class="task__file">
+                                <a class="download-link" href="#"></a>
+                                </td>
+                                <td class="task__date ">'.$masTask[$i]['date'].'</td>
+                                <td class="task__controls"></td>
+                                </tr>
+                                ';
+                                    }
+                                    else {continue;}
+                                }
+                                else{
+                                    if (strtotime($masTask[$i]['date']) - time() <=24*3600) {
+                                        $flagTaskImportant = 'task--important';
+                                    }
+                                    else {
+                                        $flagTaskImportant = '';
+                                    }
+                                    echo '<tr class="tasks__item task '.$flagTaskImportant.'">
+                                    <td class="task__select">
+                                    <label class="checkbox task__checkbox"><input class="checkbox__input visually-hidden task__checkbox" type="checkbox" value="1">
+                                    <span class="checkbox__text">'.$masTask[$i]['task'].'</span>
+                                    </label>
+                                    </td>
+                                    <td class="task__file">
+                                    <a class="download-link" href="#"></a>
+                                    </td>
+                                    <td class="task__date">'.$masTask[$i]['date'].'</td>
+                                    <td class="task__controls"></td>
+                                    </tr>
+                                    ';
+                                };
+                            };
+                            ?>                    
+                </table>
+            </main>
